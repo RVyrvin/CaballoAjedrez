db796005b03f8650725162032106f3e79f394afa
hamster <hamster@pc>
2018-11-21 13:47:22 +0100

    7. se ha creado el constructor para la clase Posicion con los parametros

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -3,9 +3,24 @@ package org.iesalandalus.programacion.caballoajedrez;
 public class Posicion {
 	
 	private int fila;
-	private char columna;
+	private char columna;	
 	
 	
+	public Posicion(int fila, char columna) {
+		
+		super();
+		
+		if (fila < 1 || fila > 8)
+			this.fila = fila;
+		else 
+			throw new IllegalArgumentException("La fila teiene ser ente 1 y 8, inclusive");
+		
+		if ( columna < 'a' || columna > 'h')
+			this.columna = columna;
+		else
+			throw new IllegalArgumentException("La columna tiene que ser entre 'a' y 'h', inclusive");
+	}
+
 	public int getFila() {
 		return fila;
 	}
