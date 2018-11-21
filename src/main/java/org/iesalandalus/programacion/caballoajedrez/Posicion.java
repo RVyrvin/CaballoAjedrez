ee4056df2a7f305e76b2446f16fd40c765a866b9
hamster <hamster@pc>
2018-11-21 13:43:58 +0100

    6. añadidos los metodos set y get para filas y columnas

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -4,5 +4,28 @@ public class Posicion {
 	
 	private int fila;
 	private char columna;
+	
+	
+	public int getFila() {
+		return fila;
+	}
+	
+	public void setFila(int fila) {
+		if (fila < 1 || fila > 8)
+			this.fila = fila;
+		else 
+			throw new IllegalArgumentException("La fila teiene ser ente 1 y 8, inclusive");
+	}
+	
+	public char getColumna() {
+		return columna;
+	}
+	
+	public void setColumna(char columna) {
+		if ( columna < 'a' || columna > 'h')
+			this.columna = columna;
+		else
+			throw new IllegalArgumentException("La columna tiene que ser entre 'a' y 'h', inclusive");
+	}	
 
 }
